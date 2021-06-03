7689b12cdd44ee3f07d5e9a5e611549fe4cba611
Klejdi <klejdi@ymail.com>
2021-06-02 22:03:51 -0500

    final

--- lesson12/asgnm2/index.js
+++ lesson12/asgnm2/index.js
@@ -1,26 +1,43 @@
-//this is the asgm ---------------------------------------------------
-let fibonacci = new Promise(function(resolve, reject) {
-    // console.log(resolve + reject);
-});
-fibonacci.then(fib).catch(err);
+// //this is the asgm ---------------------------------------------------
 
-function fib(number) {
-    if (number <= 2) {
-        return 1;
-    } else {
-        return fib(number - 1) + fib(number - 2);
+async function fibonacci(num) {
+    var a = 1,
+        b = 0,
+        temp;
+    while (num >= 0) {
+        temp = a;
+        a = a + b;
+        b = temp;
+        num--;
     }
+    return b;
 }
+console.log("start")
+fibonacci(41).then((res) => { console.log(res) });
+console.log("end")
 
-function err(err) {
-    console.log(err);
-}
-console.log(fib(10));
+// let fibonacci = new Promise(function(resolve, reject) {
+//     // console.log(resolve + reject);
+// });
+// fibonacci.then(fib).catch(err);
+
+// function fib(number) {
+//     if (number <= 2) {
+//         return 1;
+//     } else {
+//         return fib(number - 1) + fib(number - 2);
+//     }
+// }
+
+// function err(err) {
+//     console.log(err);
+// }
+// console.log(fib(10));
 
 
 //-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
 // let fibonacc = new Promise(function(resolve, reject) {
-//     console.log(resolve + reject);
+//     // console.log(resolve + reject);
 // });
 
 // function fib1(number) {
@@ -33,16 +50,21 @@ console.log(fib(10));
 
 // fibonacc.then(result).catch(err1);
 // var a = 0;
+
 // async function result() {
 
-//     a = await fib1(41);
+//     console.log(await fib1(41));
 
 
 // }
-// // console.log(a);
+// console.log(a);
 
 // function err1(err1) {
 //     console.log(err1);
 // }
+// console.log("start")
+
 // result();
+// console.log("end")
+
 // console.log("print this after the result");
